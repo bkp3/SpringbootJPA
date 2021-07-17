330ecd8b06c8924d1be23540405425ff76fb4dd3
bkp3 <bholakumar.7372@gmail.com>
2021-07-17 22:22:58 +0530

    db configuration in application.properies file

--- SpringbootJPA/src/main/java/com/jpa/test/SpringbootJpaApplication.java
+++ SpringbootJPA/src/main/java/com/jpa/test/SpringbootJpaApplication.java
@@ -2,12 +2,30 @@ package com.jpa.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
 
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJpaApplication.class, args);
+		
+		
+		ApplicationContext context=SpringApplication.run(SpringbootJpaApplication.class, args);
+		
+		UserRepository userRepository = context.getBean(UserRepository.class);
+		
+		User user = new User();
+		user.setName("Rakesh Kumar");
+		user.setCity("Delhi");
+		user.setStatus("I am a Student");
+		
+		User user1=userRepository.save(user);
+		System.out.println(user1);
+		
+		
 	}
 
 }
